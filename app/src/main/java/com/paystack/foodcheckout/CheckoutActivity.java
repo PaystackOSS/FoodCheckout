8bbde003c0b929ba5b95338337dfc24bfe05e0b2
Damilola Odujoko <damilola.odujoko@paystack.com>
2021-06-10 15:20:29 +0100

    🚀 Chore: Create starter template

--- app/src/main/java/com/paystack/foodcheckout/CheckoutActivity.java
+++ app/src/main/java/com/paystack/foodcheckout/CheckoutActivity.java
@@ -31,15 +31,12 @@ public class CheckoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
-        initializePaystack();
+        // TODO - 3: Call the initializePaystack method
         initializeFormVariables();
     }
 
-    private void initializePaystack() {
-        PaystackSdk.initialize(getApplicationContext());
-        PaystackSdk.setPublicKey(BuildConfig.PSTK_PUBLIC_KEY);
-    }
-
+    // TODO - 1: Create the initializePaystack method
+    // TODO - 2: Initialize the Paystack SDK in the initializePaystack method
 
     private void initializeFormVariables() {
         mCardNumber = findViewById(R.id.til_card_number);
@@ -74,47 +71,14 @@ public class CheckoutActivity extends AppCompatActivity {
     }
 
     private void performCharge() {
-        Intent intent = getIntent();
+        // TODO - 4: Flesh out the performCharge method
+        // TODO - 5: Get the cardNumber, cardExpiry and cvv from the checkout form
+        // TODO - 6: Parse the cardNumber, cardExpiry and cvv to ensure they are ready for charge
+        // TODO - 7: Create a card object with the cardNumber, cardExpiry and cvv
 
-        String cardNumber = mCardNumber.getEditText().getText().toString();
-        String cardExpiry = mCardExpiry.getEditText().getText().toString();
-        String cvv = mCardCVV.getEditText().getText().toString();
-
-        String[] cardExpiryArray = cardExpiry.split("/");
-        int expiryMonth = Integer.parseInt(cardExpiryArray[0]);
-        int expiryYear = Integer.parseInt(cardExpiryArray[1]);
-        int amount = intent.getIntExtra(getString(R.string.meal_name), 0);
-        amount *= 100;
-
-        Card card = new Card(cardNumber, expiryMonth, expiryYear, cvv);
-
-        Charge charge = new Charge();
-        charge.setAmount(amount);
-        charge.setEmail("customer@email.com");
-        charge.setCard(card);
-
-        PaystackSdk.chargeCard(this, charge, new Paystack.TransactionCallback() {
-            @Override
-            public void onSuccess(Transaction transaction) {
-                parseResponse(transaction.getReference());
-            }
-
-            @Override
-            public void beforeValidate(Transaction transaction) {
-                Log.d("Main Activity", "beforeValidate: " + transaction.getReference());
-            }
-
-            @Override
-            public void onError(Throwable error, Transaction transaction) {
-                Log.d("Main Activity", "onError: " + error.getLocalizedMessage());
-                Log.d("Main Activity", "onError: " + error);
-            }
-
-        });
+        // TODO - 8: Create a charge object
+        // TODO - 9: Charge card
     }
 
-    private void parseResponse(String transactionReference) {
-        String message = "Payment Successful - " + transactionReference;
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
+    // TODO - 10: Create a parseResponse method to parse a successful response
 }
