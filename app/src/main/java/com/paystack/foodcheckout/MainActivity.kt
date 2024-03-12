abf174dc2507a65cbaa56d648ef714ff3bd87fac
Damilola Odujoko <damilola.odujoko@paystack.com>
2024-03-12 11:00:47 +0100

    Chore: Migrate Kotlin sample to use new SDK

--- app/src/main/java/com/paystack/foodcheckout/MainActivity.kt
+++ app/src/main/java/com/paystack/foodcheckout/MainActivity.kt
@@ -2,22 +2,53 @@ package com.paystack.foodcheckout
 
 import androidx.appcompat.app.AppCompatActivity
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.paystack.android_sdk.core.Paystack
+import com.paystack.android_sdk.ui.paymentsheet.PaymentSheet
+import com.paystack.android_sdk.ui.paymentsheet.PaymentSheetResult
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var paymentSheet: PaymentSheet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val price  = 2000
+        Paystack.builder()
+            .setPublicKey("pk_test_xxxx")
+            .build()
+        paymentSheet = PaymentSheet(this, ::paymentComplete)
+
         val mCheckout: Button = findViewById(R.id.btn_checkout)
         mCheckout.setOnClickListener {
-            val intent = Intent(this, CheckoutActivity::class.java).apply {
-                putExtra(getString(R.string.meal_name), price)
-            }
-            startActivity(intent)
+            makePayment()
         }
+    }
+
+    private fun makePayment() {
+        // Pass access_code from transaction initialize call
+        paymentSheet.launch("br6cgmvflhn3qtd")
+    }
+
+
+    private fun paymentComplete(paymentSheetResult: PaymentSheetResult) {
+        val message = when (paymentSheetResult) {
+            PaymentSheetResult.Cancelled -> "Cancelled"
+            is PaymentSheetResult.Failed -> {
+                Log.e("Something went wrong", paymentSheetResult.error.message.orEmpty(), paymentSheetResult.error)
+                paymentSheetResult.error.message ?: "Failed"
+            }
+
+            is PaymentSheetResult.Completed -> {
+                // Returns the transaction reference  PaymentCompletionDetails(reference={TransactionRef})
+                Log.d("Payment successful", paymentSheetResult.paymentCompletionDetails.toString())
+                "Successful"
+            }
+        }
+
+        Toast.makeText(this, "Payment $message", Toast.LENGTH_SHORT).show()
     }
 }
